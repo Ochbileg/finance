9c435d71c196f741f3e6a8b8a49b5a152ea710cc
ochbileg <ochbileg888@gmail.com>
2021-04-24 22:20:16 +0800

    68-r hicheel hurtel uzsen bolno

--- app.js
+++ app.js
@@ -29,31 +29,65 @@ var uiController = (function () {
 
 var financeController = (function () {
   //
+  var Income = function (id, description, value) {
+    this.id = id;
+    this.description = description;
+    this.value = value;
+  };
+
+  var Expense = function (id, description, value) {
+    this.id = id;
+    this.description = description;
+    this.value = value;
+  };
+
+  var data = {
+    allitems: {
+      inc: [],
+      exp: [],
+    },
+
+    totals: {
+      inc: 0,
+      exp: 0,
+    },
+  };
 })();
 
 // Програмын холбогч компютер
 var appController = (function (uiCtrl, fnCtrl) {
   // add darhad ajillah function
   var ctrlAddItem = function () {
-    console.log(uiCtrl.getInput().value);
+    console.log(uiCtrl.getInput());
     // Oruulah ugugdliig olj avna
     // Olj avsan ugugdluudee sanhuugiin controllruu shiljuulj hadgaldag bolgono
     // olj avsan medeeliig tohiroh orlogo zarlaga deer haruulna
     // tusviig tootsolno
     // etsiin uldegdel tootsoog haruulna
   };
-  // uiController dotor bga dom medeelliig damjuulah zorilgotoi
-  var DOM = uiCtrl.getDomStrings();
 
-  // keyboard bolon icon event listeneeruud
-  document.querySelector(DOM.addBtn).addEventListener("click", function () {
-    ctrlAddItem();
-  });
-  // Keyboard unshih zorilgotoi
-  document.addEventListener("keyup", (event) => {
-    if (event.key === "Enter" || event.which === 13) {
+  var setupEventListeners = function () {
+    // uiController dotor bga dom medeelliig damjuulah zorilgotoi
+    var DOM = uiCtrl.getDomStrings();
+    // keyboard bolon icon event listeneeruud
+    document.querySelector(DOM.addBtn).addEventListener("click", function () {
       ctrlAddItem();
-    }
-  });
+    });
+    // Keyboard unshih zorilgotoi
+    document.addEventListener("keyup", (event) => {
+      if (event.key === "Enter" || event.which === 13) {
+        ctrlAddItem();
+      }
+    });
+  };
+  // public function huvisagch nar end hadgalnaa
+  return {
+    init: function () {
+      console.log("app ehellee");
+      setupEventListeners();
+    },
+  };
   // ui bolon finance functionii medeelliig access hiihed zoriulagdsan
 })(uiController, financeController);
+
+appController.init();
